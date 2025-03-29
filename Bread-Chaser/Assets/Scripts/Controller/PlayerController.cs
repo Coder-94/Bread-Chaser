44b7535a0543124f5a13a7e701133c55c465ecd1
Coder-94 <bhtoby25@gmail.com>
2025-03-29 21:04:50 +0900

    jump

--- Bread-Chaser/Assets/Scripts/Controller/PlayerController.cs
+++ Bread-Chaser/Assets/Scripts/Controller/PlayerController.cs
@@ -1,28 +1,9 @@
+using System.Collections;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : PlayerMover
 {
-    //x좌표 이동범위
-    private float       maxXwidth = 1.5f;
-    //회피 소모 거리
-    private float       sideMovesSpd = 2.0f;
-    private bool        isMove = false;
 
-    private float       originY = -0.09f;
-    private float       gravity = -9.81f;
-    private float       moveTimeY = 0.3f;
-    private bool        isJump = true;
-
-    [SerializeField]
-    private float       moveSpeed = 20.0f;
-
-    private float       deathY = -3f;
-    private Rigidbody   rigid;
-
-    private void Start()
-    {
-        rigid = GetComponent<Rigidbody>();
-    }
 
     private void Update()
     {
@@ -35,16 +16,4 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    public void MovetoX(int x)
-    {
-        if (isMove == false)
-            return;
-
-        if( gameObject.transform.position.x > maxXwidth || gameObject.transform.position.x < -maxXwidth)
-        {
-            Vector3 pos = transform.position;
-            pos.x = Mathf.Clamp(pos.x, -maxXwidth, maxXwidth); // x 값 제한
-            transform.position = pos;
-        }
-    }
 }
