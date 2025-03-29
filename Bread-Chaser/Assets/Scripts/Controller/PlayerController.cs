2728d3522c6383e0bf1acf5f65ae34141ada5137
Coder-94 <bhtoby25@gmail.com>
2025-03-29 18:48:33 +0900

    basic move

--- Bread-Chaser/Assets/Scripts/Controller/PlayerController.cs
+++ Bread-Chaser/Assets/Scripts/Controller/PlayerController.cs
@@ -2,15 +2,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //x좌표 이동범위
+    private float       maxXwidth = 1.5f;
+    //회피 소모 거리
+    private float       sideMovesSpd = 2.0f;
+    private bool        isMove = false;
+
+    private float       originY = -0.09f;
+    private float       gravity = -9.81f;
+    private float       moveTimeY = 0.3f;
+    private bool        isJump = true;
+
+    [SerializeField]
+    private float       moveSpeed = 20.0f;
+
+    private float       deathY = -3f;
+    private Rigidbody   rigid;
+
+    private void Start()
     {
-        
+        rigid = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+        //rigid.MovePosition(transform.position + transform.forward * moveSpeed * Time.deltaTime);
+
+        if (transform.position.y < deathY)
+        {
+            Debug.Log("hp -1");
+        }
+    }
+
+    public void MovetoX(int x)
+    {
+        if (isMove == false)
+            return;
+
+        if( gameObject.transform.position.x > maxXwidth || gameObject.transform.position.x < -maxXwidth)
+        {
+            Vector3 pos = transform.position;
+            pos.x = Mathf.Clamp(pos.x, -maxXwidth, maxXwidth); // x 값 제한
+            transform.position = pos;
+        }
     }
 }
